93a10600edd46294cd34b90917a654249ca9d443
Jan Świątek <jan.swiatek@protonmail.com>
2021-12-19 13:10:49 +0100

    Cepstrum template

--- inf145250_inf145390.py
+++ inf145250_inf145390.py
@@ -1,1 +1,44 @@
-print('afs')
+import numpy
+import soundfile
+import sys
+
+from matplotlib import pyplot as plt
+
+
+def main():
+    filename = sys.argv[1]
+
+    data, rate = soundfile.read(f"{filename}")
+
+    # s(n)
+    signal = data[:, 0]
+    plt.plot(signal)
+    plt.show()
+
+    signal = signal * numpy.hamming(len(signal))
+    plt.plot(signal)
+    plt.show()
+
+    # x(n)
+    signal = numpy.fft.fft(signal)
+    plt.plot(signal)
+    plt.show()
+
+    # |x(w)|
+    signal = numpy.abs(signal)
+    plt.plot(signal)
+    plt.show()
+
+    # log(x(w))
+    signal = numpy.log(signal)
+    plt.plot(signal)
+    plt.show()
+
+    # c(n)
+    signal = numpy.fft.ifft(signal)
+    plt.plot(signal)
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
