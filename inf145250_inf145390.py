b47fba6918466cb5418f159806d517e8ceb5955a
natalia-szymczyk <nataliaszymczyk17@gmail.com>
2021-12-20 22:51:56 +0100

    final commit

--- inf145250_inf145390.py
+++ inf145250_inf145390.py
@@ -6,20 +6,18 @@ from scipy.signal import decimate
 
 def main():
     file = sys.argv[1]
-
     data, rate = soundfile.read(file)
+
     y = [np.mean(value) for value in data]
 
     x = np.fft.fftfreq(len(data), 1 / rate)
-
     y = y * np.hamming(len(y))
     y = np.fft.fft(y)
     y = abs(y)
 
     hps_y = y.copy()
     for k in range(2, 5):
-        tmp_d = decimate(y, k)
-        hps_y[:len(tmp_d)] *= tmp_d
+        hps_y[:len(decimate(y, k))] *= decimate(y, k)
 
     hps_y[:10] = 0
 
@@ -29,36 +27,17 @@ def main():
 
     result = x[mask][np.argmax(hps_y[mask])]
 
-    #
-    # print(mask.shape)
-    # print(hps_y.shape)
-
-    # mask2 = np.ndarray(hps_y.shape, dtype=bool)
-    #
-    # for i, value in enumerate(hps_y):
-    #     if i < male[0] or i > female[1]:
-    #         mask2[i] = False
-    #     else:
-    #         mask2[i] = True
-
-    # print(len(mask2))
-    # print(mask == mask2)
-    # result = x[np.argmax(voice)]
-
-    # print(hps_y[mask])
-    # print(voice)
-
     answer = 'M'
     if female[0] <= result <= female[1]:
         answer = 'K'
 
     correct_answer = file[-5]
 
-    print(answer)
+    print(answer, result)
 
 
 if __name__ == '__main__':
     try:
         main()
-    except:
+    except Exception as ex:
         print('K')
